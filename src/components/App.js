d48c551fd6518bf682b11771f0c9940539f10f1a
zblockdoc <zblockdoc@gmail.com>
2023-11-06 09:17:42 -0500

    Select Pairs

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import {
 } from '../store/interactions';
 
 import Navbar from './Navbar';
+import Markets from './Markets';
 
 function App() {
   const dispatch = useDispatch();
@@ -67,7 +68,7 @@ function App() {
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+          <Markets/>
 
           {/* Balance */}
 
